9031b4f41efdbcf0fa710eb26ce1af529ac9b541
my life kinda sucks <nguyendtgiang@gmail.com>
2022-11-18 13:28:55 +0700

    Refactor response

--- src/response/mod.rs
+++ src/response/mod.rs
@@ -21,20 +21,20 @@ pub struct Response {
 
 impl Response {
     pub fn new(user: User, files: Vec<File>, folders: Vec<Folder>) -> Result<Self> {
-        let mut response_files = vec![];
-        for file in files {
-            response_files.push(file.into_response()?)
-        }
+        let files = files
+            .into_iter()
+            .map(|f| f.into_response())
+            .collect::<Result<_>>()?;
 
-        let mut response_folders = vec![];
-        for folder in folders {
-            response_folders.push(folder.into_response()?)
-        }
+        let folders = folders
+            .into_iter()
+            .map(|f| f.into_response())
+            .collect::<Result<_>>()?;
 
         Ok(Self {
             user: user.into_response()?,
-            files: response_files,
-            folders: response_folders,
+            files,
+            folders,
         })
     }
 }
