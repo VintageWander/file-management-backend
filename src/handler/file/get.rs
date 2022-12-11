4270d768c31c655b3b1a05290b44ad9ae7911b9a
my life kinda sucks <nguyendtgiang@gmail.com>
2022-12-11 15:19:09 +0700

    default search route are now queryable

--- src/handler/file/get.rs
+++ src/handler/file/get.rs
@@ -1,26 +1,61 @@
-use salvo::{handler, Depot};
+use std::{collections::HashMap, str::FromStr};
+
+use mongodb::bson::oid::ObjectId;
+use salvo::{handler, Depot, Request};
 
 use crate::{
-    helper::depot::{get_file_service, get_file_version_service, get_param_file, get_user_service},
+    helper::{
+        cookie::get_cookie_user_id_option,
+        depot::{get_file_service, get_file_version_service, get_param_file, get_user_service},
+    },
     response::FinalFileResponse,
     web::Web,
     WebResult,
 };
 
 #[handler]
-pub async fn get_public_files_handler(depot: &mut Depot) -> WebResult {
-    let files = get_file_service(depot)?.get_public_files().await?;
+pub async fn get_files_handler(req: &mut Request, depot: &mut Depot) -> WebResult {
+    // Get the query data
+    let mut queries = req
+        .queries()
+        .iter()
+        .map(|i| (i.0.clone(), i.1.clone()))
+        .collect::<HashMap<_, _>>();
+
+    let cookie_user_id_option = get_cookie_user_id_option(depot);
+
+    let file_service = get_file_service(depot)?;
+    let user_service = get_user_service(depot)?;
+
+    let files = match (cookie_user_id_option, queries.get(&"owner".to_string())) {
+        (Some(cookie_user_id), Some(query_owner)) => {
+            let owner_id = ObjectId::from_str(query_owner)?;
+            if *cookie_user_id == owner_id {
+                file_service.get_files_by_map(&queries)
+            } else {
+                queries.insert("visibility".to_string(), "public".to_string());
+                file_service.get_files_by_map(&queries)
+            }
+        }
+        _ => {
+            queries.insert("visibility".to_string(), "public".to_string());
+            file_service.get_files_by_map(&queries)
+        }
+    };
+
+    let files = files.await?;
+
     let mut responses = vec![];
 
     for file in files {
-        let owner = get_user_service(depot)?.get_user_by_id(&file.owner).await?;
+        let owner = user_service.get_user_by_id(&file.owner).await?;
         let versions = get_file_version_service(depot)?
             .get_versions_by_file_id(&file.id)
             .await?;
         responses.push(FinalFileResponse::new(file, owner, versions)?)
     }
 
-    Ok(Web::ok("Get public files successfully", responses))
+    Ok(Web::ok("Get files successfully", responses))
 }
 
 #[handler]
--- src/handler/folder/get.rs
+++ src/handler/folder/get.rs
@@ -1,7 +1,11 @@
+use std::{collections::HashMap, str::FromStr};
+
+use mongodb::bson::oid::ObjectId;
 use salvo::{handler, Depot, Request};
 
 use crate::{
     helper::{
+        cookie::get_cookie_user_id_option,
         depot::{get_folder_service, get_param_folder, get_user_service},
         param::get_param_folder_id,
     },
@@ -11,24 +15,41 @@ use crate::{
 };
 
 #[handler]
-pub async fn get_public_folders_handler(req: &mut Request, depot: &mut Depot) -> WebResult {
-    let folders = match req.query::<&str>("position") {
-        None => get_folder_service(depot)?.get_public_folders().await?,
-        Some(query) => {
-            get_folder_service(depot)?
-                .get_public_folders_by_prefix_position(query)
-                .await?
+pub async fn get_folders_handler(req: &mut Request, depot: &mut Depot) -> WebResult {
+    // Get the query data
+    let mut queries = req
+        .queries()
+        .iter()
+        .map(|i| (i.0.clone(), i.1.clone()))
+        .collect::<HashMap<_, _>>();
+
+    let cookie_user_id_option = get_cookie_user_id_option(depot);
+
+    match (cookie_user_id_option, queries.get(&"owner".to_string())) {
+        (Some(cookie_user_id), Some(query_owner)) => {
+            if *cookie_user_id != ObjectId::from_str(query_owner)? {
+                queries.insert("visibility".to_string(), "public".to_string());
+            }
+        }
+        _ => {
+            queries.insert("visibility".to_string(), "public".to_string());
         }
     };
+
+    let folders = get_folder_service(depot)?
+        .get_folders_by_map(&queries)
+        .await?;
+
     let mut responses = vec![];
+
     for folder in folders {
-        let owner = folder.owner;
-        responses.push(FinalFolderResponse::new(
-            folder,
-            get_user_service(depot)?.get_user_by_id(&owner).await?,
-        )?)
+        let owner = get_user_service(depot)?
+            .get_user_by_id(&folder.owner)
+            .await?;
+        responses.push(FinalFolderResponse::new(folder, owner)?)
     }
-    Ok(Web::ok("Get public folders successfully", responses))
+
+    Ok(Web::ok("Get folders successfully", responses))
 }
 
 #[handler]
